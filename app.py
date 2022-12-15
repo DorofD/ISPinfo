2f5776c1672ab6cb0f574952bbecb87c76f7ad4b
DorofeevE <legion.0@yandex.ru>
2022-12-15 19:31:22 +0300

    маршрутизация поисковых запросов, результат поиска

--- app.py
+++ app.py
@@ -6,21 +6,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    print(url_for('index'))
     return render_template('index.html')
 
 @app.route('/pid', methods=('GET', 'POST'))
 def pid():
+    result = 'сасать, нет такого магазина, лох'
     if request.method == 'POST':
         if request.form['pid'] == '228':
-            print('228')
-    return render_template('index.html')
+            result = request.form['pid']
+ 
+    return render_template('searchResult.html', result = result)
 
 @app.route('/shop', methods=('GET', 'POST'))
 def shop():
+    result = 'сасать, нет такого магазина, лох'
     if request.method == 'POST':
-        if request.form['shop'] == 'сас':
-            print('сас')
-    return render_template('index.html')
+        if request.form['shop'] == 'магазин':
+            result = request.form['shop']
+ 
+    return render_template('searchResult.html', result = result)
 
 if __name__ == '__main__':
     app.run(debug=True)
