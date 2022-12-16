8a518610b30bd6bbc4443422730fc4698f949c97
DorofD <Legion.0@yandex.ru>
2022-12-16 16:04:53 +0300

    правка вёрстки

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import sqlite3, os, math, time, re
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def index():
     print(url_for('index'))
-    return render_template('index.html')
+    return render_template('index.html', class1 = 'active', class2 = '', class3 = '')
 
 @app.route('/pid', methods=('GET', 'POST'))
 def pid():
@@ -15,8 +17,7 @@ def pid():
     if request.method == 'POST':
         if request.form['pid'] == '228':
             result = request.form['pid']
- 
-    return render_template('searchResult.html', result = result)
+    return render_template('searchResult.html', result = result, class1 = 'active', class2 = '', class3 = '')
 
 @app.route('/shop', methods=('GET', 'POST'))
 def shop():
@@ -24,8 +25,15 @@ def shop():
     if request.method == 'POST':
         if request.form['shop'] == 'магазин':
             result = request.form['shop']
- 
-    return render_template('searchResult.html', result = result)
+    return render_template('searchResult.html', result = result, class1 = 'active', class2 = '', class3 = '')
+
+@app.route('/update')
+def update():
+    return render_template('update.html', class1 = '', class2 = 'active', class3 = '')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', class1 = '', class2 = '', class3 = 'active')
 
 if __name__ == '__main__':
     app.run(debug=True)
