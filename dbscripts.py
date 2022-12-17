a1dba94213d8418df7647b01d449c855ad510381
DorofD <Legion.0@yandex.ru>
2022-12-17 20:43:54 +0300

    изменение формата хранения данных, подгонка форм

--- dbscripts.py
+++ dbscripts.py
@@ -3,16 +3,20 @@ import sqlite3 as sq
 def create_db():
     conn = sq.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS shops (
-    pid	INTEGER PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE
-        )""")
-    cursor.execute("""CREATE TABLE IF NOT EXISTS contracts (
-    ptid INTEGER NOT NULL,
-    type TEXT NOT NULL,
-    number TEXT NOT NULL,
-    sdemail TEXT
-        )""")
+    query = """CREATE TABLE IF NOT EXISTS contracts (
+        pid INTEGER NOT NULL,
+        shop_name TEXT NOT NULL,
+        wan_type TEXT NOT NULL,
+        ip TEXT NOT NULL,
+        legal_entity TEXT NOT NULL,
+        isp TEXT NOT NULL,
+        contract TEXT NOT NULL,
+        shop_address TEXT NOT NULL,
+        sd_phone TEXT,
+        sd_email TEXT
+    )
+    """
+    cursor.execute(query)
     conn.close()
 
 def get_data():
@@ -26,8 +30,7 @@ def get_data():
 def get_contracts(pid):
     conn = sq.connect('database.db')
     cursor = conn.cursor()
-    query = f"""SELECT shops.pid, shops.name, contracts.type, contracts.number, contracts.sdemail FROM shops, contracts
-        ON contracts.ptid = shops.pid
+    query = f"""SELECT * FROM contracts
         WHERE pid = '{pid}'"""
     # try:
     cursor.execute(query)
@@ -49,3 +52,5 @@ def set_data():
     cursor.execute(f"""INSERT INTO shops (pid, name) VALUES ('{pid}', '{shop}')""")
     conn.commit()
     conn.close()
+
+create_db()
